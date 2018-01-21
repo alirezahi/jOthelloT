b797c08c7955d5e9e9e404897cc02bac2694e50a
Alireza Heydari <alireza97hi@gmail.com>
2018-01-21 17:18:20 +0330

    monte carlo algorithm added

--- src/players/MonteCarloPlayer.java
+++ src/players/MonteCarloPlayer.java
@@ -4,30 +4,230 @@ package players;
  *
  * @author Alireza Heydari
  */
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Random;
+import java.util.Date;
 
-import game.AbstractPlayer;
-import game.BoardSquare;
-import game.Move;
-import game.OthelloGame;
+import game.*;
 
 public class MonteCarloPlayer extends AbstractPlayer {
+    static int X = 1, O = -1, validate;
+
+    public static String winner;
+    public static void print(String string) {
+        System.out.println(string);
+    }
 
     public MonteCarloPlayer(int depth) {
         super(depth);
     }
 
+
+    public class Node{
+        public ArrayList<Move> unexamined;
+        public Move action;
+        public int wins = 0;
+        public int visits = 0;
+        public Node parent;
+        public ArrayList<Node> children = new ArrayList<>();
+        public int activePlayer ;
+        public Node(Node parentNode,int [][] board, Move move){
+            this.action = move;
+            this.wins = 0;
+            this.visits = 0;
+            this.parent = parentNode;
+            this.unexamined = new ArrayList<>();
+            this.children = new ArrayList<>();
+            this.activePlayer =1;
+        }
+        public Node addChild(int [][] board,int index){
+            Node node = new Node(null, board, this.unexamined.get(index));
+            node.unexamined = new ArrayList<>();
+            this.children.add(node);
+            return node;
+        }
+
+        public Node selectChild(){
+            Node selected = null;
+            double bestVal = Double.NEGATIVE_INFINITY;
+            for(int i=0;i<this.children.size();i++){
+                Node child = this.children.get(i);
+                double uctValue = child.wins / child.visits + Math.sqrt(2 * Math.log(this.visits) / child.visits);
+                if(uctValue > bestVal){
+                    selected = child;
+                    bestVal = uctValue;
+                }
+            }
+            return selected;
+        }
+
+        public Node mostVisitedNode(){
+            Node mostVisited = this.children.get(0);
+            for(int i=1;i<this.children.size();i++){
+                if(this.children.get(i).visits > mostVisited.visits){
+                    mostVisited = this.children.get(i);
+                }
+            }
+            return mostVisited;
+        }
+
+        public void update(int[] result){
+            ++this.visits;
+            this.wins += result[this.activePlayer];
+        }
+    }
     @Override
     public BoardSquare play(int[][] tab) {
-        OthelloGame jogo = new OthelloGame();
-        Random r = new Random();
-        List<Move> jogadas = jogo.getValidMoves(tab, getMyBoardMark());
-        if (jogadas.size() > 0) {
-            return jogadas.get(r.nextInt(jogadas.size())).getBardPlace();
-        } else {
-            return new BoardSquare(-1, -1);
+        OthelloGame game = new OthelloGame();
+        int maxTime=1000000000;
+        int maxIterations=1000000000;
+        Node root = new Node(null, tab, null);
+        long startTime = (new Date()).getTime();
+        long timeLimit = startTime + maxTime;
+        int blockSize = 50;
+        int nodesVisted = 0;
+
+        for(int iterations=0; iterations<maxIterations && (new Date()).getTime()<timeLimit; iterations+=blockSize) {
+            for (int i = 0; i < blockSize; ++i) {
+                Node node = root;
+                int[][] new_borad = tab.clone();
+                /* Selection */
+                while(node.unexamined.size() == 0 && node.children.size()>0){
+                    node = node.selectChild();
+//                    game.do_move()
+                }
+                /* Expansion */
+                if(node.unexamined.size() > 0){
+                    int j = (int)Math.floor(Math.random() * node.unexamined.size());
+//                    game.do_move()
+                    node = node.addChild(tab,j);
+                }
+                /* Simulation */
+                List<Move> actions =  game.getValidMoves(tab,1);
+                while(actions.size() > 0){
+                    // game.do_move()
+                    ++nodesVisted;
+                    actions = game.getValidMoves(tab,1);
+                }
+                /* Backpropagation */
+                int[] result = new int[2]; // = game.getResult();
+                while (node != null){
+                    node.update(result);
+                    node = node.parent;
+                }
+            }
         }
+
+        long duration = (new Date().getTime()) - startTime;
+
+        return new BoardSquare();
+//
+//        OthelloGame jogo = new OthelloGame();
+//        Random r = new Random();
+//        List<Move> jogadas = jogo.getValidMoves(tab, getMyBoardMark());
+//        if (jogadas.size() > 0) {
+//            return jogadas.get(r.nextInt(jogadas.size())).getBardPlace();
+//        } else {
+//            return new BoardSquare(-1, -1);
+//        }
+    }
+    public int random_play(){
+        OthelloGame game = new OthelloGame();
+        //Define player 1 class
+        AbstractPlayer player = new players.RandomPlayer(2);
+        player.setBoardMark(X);
+        player.setOpponentBoardMark(O);
+        player.setGame(game);
+        //Define player 2 class
+        AbstractPlayer player2 = new players.RandomPlayer(2);
+        player2.setBoardMark(O);
+        player2.setOpponentBoardMark(X);
+        player2.setGame(game);
+        BoardSquare boardPlace = null;
+        List<Move> moveList;
+        OthelloBoard othelloBoard = new OthelloBoard(game);
+        while (true) {
+            int result = 0;
+//           player 1 plays
+            moveList = game.getValidMoves(othelloBoard.getBoard(), player.getMyBoardMark());
+            System.out.println('5');
+            if (moveList.size() > 0) {
+                try {
+                    boardPlace = player.play(othelloBoard.getBoard());
+                    validate = game.validate_moviment(othelloBoard.getBoard(), boardPlace, player);
+                } catch (Exception e) {
+                    validate = -2;
+                    winner = player2.getClass().toString();
+                }
+            } else {
+                boardPlace = new BoardSquare(-1, -1);
+                validate = game.validate_moviment(othelloBoard.getBoard(), boardPlace, player);
+            }
+            if (validate == 0) {
+                othelloBoard.setBoard(game.do_move(othelloBoard.getBoard(), boardPlace, player));
+            } else if (validate == -1 || validate == -2) {
+                System.err.println(player.getClass().toString() + " WO: invalid play");
+                print(player.getClass().toString() + " player1 WO: invalid play");
+                break;
+            }
+
+            print("Player 1");
+            othelloBoard.print();
+
+//            check if player 1 won
+            result = game.testing_end_game(othelloBoard.getBoard(), player.getBoardMark());
+            if (result != 0) {
+                if (result == 1) {
+                    winner = player.getClass().toString();
+                    print("Player 1 Won");
+                } else {
+                    winner = "draw";
+                    print("Draw");
+                }
+                break;
+            }
+
+//           player 2
+            moveList = game.getValidMoves(othelloBoard.getBoard(), player2.getMyBoardMark());
+            if (moveList.size() > 0) {
+                try {
+                    boardPlace = player2.play(othelloBoard.getBoard());
+                    validate = game.validate_moviment(othelloBoard.getBoard(), boardPlace, player2);
+                } catch (Exception e) {
+                    validate = -2;
+                    winner = player.getClass().toString();
+                }
+            } else {
+                boardPlace = new BoardSquare(-1, -1);
+                validate = game.validate_moviment(othelloBoard.getBoard(), boardPlace, player2);
+            }
+            if (validate == 0) {
+                othelloBoard.setBoard(game.do_move(othelloBoard.getBoard(), boardPlace, player2));
+            } else if (validate == -1 || validate == -2) {
+                print("player2 WO: invalid play");
+                System.err.println(player2.getClass().toString() + " WO: invalid play");
+                break;
+            }
+            print("Player2");
+            othelloBoard.print();
+
+//            check if player 2 won
+            result = game.testing_end_game(othelloBoard.getBoard(), player2.getBoardMark());
+            if (result != 0) {
+                if (result == 1) {
+                    winner = player2.getClass().toString();
+                    print("Player 2 won");
+                } else {
+                    winner = "draw";
+                    print("Draw");
+                }
+                break;
+            }
+
+        }
+        return 0;
     }
 
 }
